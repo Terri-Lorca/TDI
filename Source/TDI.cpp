6d9c5869e7eb3967daf14e88954ea99f34f99214
Alejandro Martinez Terrones <amt871@inlumine.ual.es>
2023-04-22 12:16:38 +0200

    Trabajo parcial acabado

--- Source/TDI.cpp
+++ Source/TDI.cpp
@@ -15,17 +15,16 @@
 #include <iostream>
 
 string nombreImg, nombreImgCompleto;
-C_Image imagen, imgSobelX, imgSobelY, imgFinal;
+C_Image imagen, imgSobelX, imgSobelY, imgFinal, original;
 int umbral;
 
 void Sobelx();
 void Sobely();
 void convolucion(C_Image imagen, C_Matrix mascara, string eje);
-void SumaEjes();
-void invertirColores();
 void umbralizar(int umbral);
 void suavizadoGaussiano(C_Image imagen, double sigma);
-void magnitudYDireccionGradiente();
+void magnitudGradientes();
+void invertirColores();
 
 
 int main(int argc, char** argv)
@@ -46,16 +45,16 @@ int main(int argc, char** argv)
         exit;
     } else {
         cout << "Se ha cargado la imagen correctamente\n";
-        suavizadoGaussiano(imagen, 1.0);
+        original = imagen;
+        /*suavizadoGaussiano(imagen, 1.0);*/
         Sobelx();
         Sobely();
         cout << "Introduce el umbral\n";
-        cin >> umbral;
-        umbralizar(umbral);
         imgSobelX.WriteBMP("Prueba1.bmp");
         imgSobelY.WriteBMP("Prueba2.bmp");
         imgFinal = imagen;
-        magnitudYDireccionGradiente();
+        magnitudGradientes();
+        invertirColores();
         imgFinal.WriteBMP("Final.bmp");
     }
 }
@@ -71,6 +70,7 @@ void Sobelx() {
     Msobel(0, 2) = 1;
     Msobel(1, 2) = 2;
     Msobel(2, 2) = 1;
+
     convolucion(imagen, Msobel, "x");
 }
 
@@ -90,29 +90,21 @@ void Sobely() {
 
 void convolucion(C_Image imagen, C_Matrix mascara, string eje) {
     // Ampliar la imagen en los bordes duplicando los valores
-    C_Image ampliada(imagen.FirstRow() - 1, imagen.LastRow() + 1, imagen.FirstCol() - 1, imagen.LastCol() + 1);
-    for (int i = ampliada.FirstRow(); i <= ampliada.LastRow(); i++) {
-        for (int j = ampliada.FirstCol(); j <= ampliada.LastCol(); j++) {
-            int row = i, col = j;
-            if (i < imagen.FirstRow()) row = imagen.FirstRow();
-            if (i > imagen.LastRow()) row = imagen.LastRow();
-            if (j < imagen.FirstCol()) col = imagen.FirstCol();
-            if (j > imagen.LastCol()) col = imagen.LastCol();
-            ampliada(i, j) = imagen(row, col);
-        }
-    }
+    imagen.Resize(imagen.FirstRow() - 1, imagen.LastRow() + 1, imagen.FirstCol() - 1, imagen.LastCol() + 1);
 
     // Aplicar la máscara de convolución
-    C_Image resultado(imagen.FirstRow(), imagen.LastRow(), imagen.FirstCol(), imagen.LastCol());
-    for (int i = resultado.FirstRow(); i <= resultado.LastRow(); i++) {
-        for (int j = resultado.FirstCol(); j <= resultado.LastCol(); j++) {
+    C_Image resultado(original.FirstRow() , original.LastRow(), original.FirstCol(), original.LastCol());
+    for (int i = imagen.FirstRow() + 1; i <= imagen.LastRow() - 1; i++) {
+        for (int j = imagen.FirstCol() + 1; j <= imagen.LastCol() - 1; j++) {
             int suma = 0;
-            for (int y = i - 1; y <= i + 1; y++) {
-                for (int x = j - 1; x <= j + 1; x++) {
-                    suma += mascara(y - i + 1, x - j + 1) * ampliada(y, x);
+            for (int x = -1; x <= 1; x++) {
+                for (int y = -1; y <= 1; y++) {
+                    suma += imagen(i + x, j + y) * mascara(1 + x, 1 + y);
                 }
             }
-            resultado(i, j) = (unsigned char) suma;
+            if (suma > 255) suma = 255;
+            if (suma < 0) suma = 0;
+            resultado(i, j) = (unsigned char)suma;
         }
     }
 
@@ -127,80 +119,29 @@ void convolucion(C_Image imagen, C_Matrix mascara, string eje) {
     }
 }
 
-void magnitudYDireccionGradiente() {
-    C_Image imgMagnitud(imagen.FirstRow(), imagen.LastRow(), imagen.FirstCol(), imagen.LastCol());
-    C_Matrix angulos(imagen.FirstRow(), imagen.LastRow(), imagen.FirstCol(), imagen.LastCol());
 
-    // Calcular la magnitud y la dirección del gradiente
+
+void magnitudGradientes() {
+    // Calcular la magnitud del gradiente
     for (int i = imgSobelX.FirstRow(); i <= imgSobelX.LastRow(); i++) {
         for (int j = imgSobelX.FirstCol(); j <= imgSobelX.LastCol(); j++) {
             int gradienteX = imgSobelX(i, j);
             int gradienteY = imgSobelY(i, j);
             int magnitud = sqrt(gradienteX * gradienteX + gradienteY * gradienteY);
-            float angulo = atan2(gradienteY, gradienteX) * 180 / M_PI;
-            imgMagnitud(i, j) = (unsigned char)magnitud;
-            angulos(i, j) = angulo;
+            imgFinal(i, j) = (unsigned char)std::min(magnitud, 255);
         }
-    }
-
-    // Supresión de no máximos
-    for (int i = imgMagnitud.FirstRow() + 1; i <= imgMagnitud.LastRow() - 1; i++) {
-        for (int j = imgMagnitud.FirstCol() + 1; j <= imgMagnitud.LastCol() - 1; j++) {
-            float angulo = angulos(i, j);
-            int magnitud = imgMagnitud(i, j);
-
-            // Comparar con los píxeles vecinos en la dirección del gradiente
-            if (((angulo > -22.5 && angulo <= 22.5) || (angulo <= -157.5 || angulo > 157.5)) &&
-                (magnitud > imgMagnitud(i, j + 1) && magnitud >= imgMagnitud(i, j - 1))) {
-                imgFinal(i, j) = (unsigned char)magnitud;
-            }
-            else if (((angulo > 22.5 && angulo <= 67.5) || (angulo <= -112.5 && angulo > -157.5)) &&
-                (magnitud > imgMagnitud(i - 1, j + 1) && magnitud >= imgMagnitud(i + 1, j - 1))) {
-                imgFinal(i, j) = (unsigned char)magnitud;
-            }
-            else if (((angulo > 67.5 && angulo <= 112.5) || (angulo <= -67.5 && angulo > -112.5)) &&
-                (magnitud > imgMagnitud(i - 1, j) && magnitud >= imgMagnitud(i + 1, j))) {
-                imgFinal(i, j) = (unsigned char)magnitud;
-            }
-            else if (((angulo > 112.5 && angulo <= 157.5) || (angulo <= -22.5 && angulo > -67.5)) &&
-                (magnitud > imgMagnitud(i - 1, j - 1) && magnitud >= imgMagnitud(i + 1, j + 1))) {
-                imgFinal(i, j) = (unsigned char)magnitud;
-            }
-            else {
-                imgFinal(i, j) = 0;
-            }
-        }
-    }
-
-    // Copiar los bordes de la imagen original
-    for (int i = imgMagnitud.FirstRow(); i <= imgMagnitud.LastRow(); i++) {
-        imgFinal(i, imgMagnitud.FirstCol()) = imgMagnitud(i, imgMagnitud.FirstCol());
-        imgFinal(i, imgMagnitud.LastCol()) = imgMagnitud(i, imgMagnitud.LastCol());
-    }
-    for (int j = imgMagnitud.FirstCol(); j <= imgMagnitud.LastCol(); j++) {
-        imgFinal(imgMagnitud.FirstRow(), j) = imgMagnitud(imgMagnitud.FirstRow(), j);
-        imgFinal(imgMagnitud.LastRow(), j) = imgMagnitud(imgMagnitud.LastRow(), j);
     }
 }
 
-
-
-
 void umbralizar(int umbral) {
 
-    for (int i = imagen.FirstRow(); i <= imagen.LastRow(); i++) {
-        for (int j = imagen.FirstCol(); j <= imagen.LastCol(); j++) {
-            if (imgSobelX(i, j) > umbral) {
-                imgSobelX(i, j) = 255; // Invierte los colores aquí
+    for (int i = imgFinal.FirstRow(); i <= imgFinal.LastRow(); i++) {
+        for (int j = imgFinal.FirstCol(); j <= imgFinal.LastCol(); j++) {
+            if (imgFinal(i, j) > umbral) {
+                imgFinal(i, j) = 0; // Invierte los colores aquí
             }
             else {
-                imgSobelX(i, j) = 0;
-            }
-            if (imgSobelY(i, j) > umbral) {
-                imgSobelY(i, j) = 255; // Invierte los colores aquí
-            }
-            else {
-                imgSobelY(i, j) = 0;
+                imgFinal(i, j) = 255;
             }
         }
     }
@@ -227,5 +168,15 @@ void suavizadoGaussiano(C_Image imagen, double sigma) {
     }
 
     convolucion(imagen, filtroGaussiano, "suavizado");
+}
+
+void invertirColores() {
+    for (int i = imagen.FirstRow() + 1; i <= imagen.LastRow() - 1; i++) {
+        for (int j = imagen.FirstCol() + 1; j <= imagen.LastCol() - 1; j++) {
+            int sum = 255 - imgFinal(i, j);
+                if (sum < 0) sum = sum *  -1;
+                imgFinal(i, j) = sum;
+        }
+    }
 }
 
